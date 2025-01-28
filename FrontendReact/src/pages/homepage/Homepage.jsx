ca024381b391d8211f5def2a3cf32d7b02fe7586
lulo97 <luongpysl2@gmail.com>
2025-01-28 20:41:29 +0700

    Add table to subject page

--- FrontendReact/src/pages/homepage/Homepage.jsx
+++ FrontendReact/src/pages/homepage/Homepage.jsx
@@ -1,11 +1,9 @@
 import { Button } from '@mantine/core';
-import { HeaderSimple } from '../../layouts/Header';
 
 export function Homepage() {
 
     return (
         <div>
-            <HeaderSimple></HeaderSimple>
             <Button variant="filled">Button</Button>
         </div>
     );
--- FrontendReact/src/pages/subjects/Subjects.jsx
+++ FrontendReact/src/pages/subjects/Subjects.jsx
@@ -1,5 +1,45 @@
-export function Subjects() {
-    return <div>123</div>
-}
+import { useState } from "react";
+import { Layout } from "../../layouts/Layout";
+import { Button } from "@mantine/core";
+import { Table } from "@mantine/core";
 
- 
+export function Subjects() {
+    const [data, setData] = useState([]);
+
+    async function fetchData() {
+        const result_fetch = await fetch("http://localhost:1337/subjects", {
+            method: "GET",
+        });
+        const result_json = await result_fetch.json();
+        const { data, error_code, error_message } = result_json;
+        setData(data);
+    }
+
+    const rows = data.map((element) => (
+        <Table.Tr key={element.id}>
+            <Table.Td>{element.id}</Table.Td>
+            <Table.Td>{element.name}</Table.Td>
+            <Table.Td>{element.description}</Table.Td>
+            <Table.Td>{element.parent_id}</Table.Td>
+        </Table.Tr>
+    ));
+
+    return (
+        <Layout>
+            <div>
+                <Button onClick={fetchData}>FetchData</Button>
+                <Table striped highlightOnHover withTableBorder withColumnBorders>
+                    <Table.Thead>
+                        <Table.Tr>
+                            <Table.Th>Id</Table.Th>
+                            <Table.Th>Name</Table.Th>
+                            <Table.Th>Description</Table.Th>
+                            <Table.Th>ParentId</Table.Th>
+                        </Table.Tr>
+                    </Table.Thead>
+                    <Table.Tbody>{rows}</Table.Tbody>
+                </Table>
+            </div>
+        </Layout>
+    );
+}
